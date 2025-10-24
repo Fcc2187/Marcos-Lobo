6d6eedf0cc6bb7bd4fb22cc5e883a3d044a4322b
Fcc2187 <fcc3@cesar.school>
2025-10-24 08:43:22 -0300

    feat: added new analysis

--- portfolio-marcos/src/blog-data/posts.js
+++ portfolio-marcos/src/blog-data/posts.js
@@ -2,6 +2,68 @@
 
 export const posts = [
   {
+    slug: 'renome-nao-e-rendimento',
+    title: 'RENOME NÃO É RENDIMENTO💡',
+    subtitle: 'A armadilha do "ganho midiático" e a falta de paciência com o processo no futebol brasileiro',
+    category: 'Desempenho de atleta',
+    date: '24 de outubro de 2025',
+    author: 'Marcos Lobo',
+    content: `
+    <p>Muitos dos clubes brasileiros estão acostumados a colocar para debaixo do tapete seus erros e escolhas ruins ao "calar" seus torcedores de redes sociais com escolhas de jogadores renomados, visando apenas ganho midiatico.</p>
+    <p>Nós somos um dos maiores formadores e potenciais de atletas profissionais do mundo; porém, não sabemos fazer a utilização correta, pois muitos desses atletas não têm o perfil ou o status que os torcedores esperam.</p>
+    <h2>O talento que o Brasil não quis esperar</h2>
+    <p align="center">
+        <img src="/alisson-santos.jpg" alt="Jogador Alisson Santos do Sporting" style="width: 100%; max-width: 550px; height: auto; margin: 20px auto; display: block; border-radius: 8px;">
+    </p>
+    <p>Alisson Santos é mais um belo exemplo de como um olhar diferente e uma oportunidade podem te fazer diferente: da série C em agosto de 2024 para 2 gols na Champions League em outubro de 2025. O que eles viram que nós não vimos? Ele é só mais um caso dos milhares que poderia citar. </p>
+    <p>Uma coisa eu tenho certeza: não temos paciência com o processo de integração desse tipo de jogador, pois os minutos que ele poderia ter ficam a cargo de jogadores renomados com baixo rendimento. Temos urgentemente que desassociar o jogador + rendimento = nome badalado. O futebol é feito de jogadores que passam por processos; estamos fazendo esse ciclo de forma invertida.</p>
+    <p>O futebol, assim como na vida, existem processos de maturação e de aprendizado; quanto mais pacientes formos com os nossos atletas, mais positivos serão os resultados em campo e fora dele no futuro.</p>
+    `
+  },
+  { 
+    slug: 'o-improvavel-mirassol',
+    title: 'O IMPROVÁVEL MIRASSOL 🌞',
+    subtitle: 'A ascensão surpreendente do Mirassol no futebol brasileiro',
+    category: 'Análise de Clubes',
+    date: '26 de Setembro de 2025',
+    author: 'Marcos Lobo',
+    content: `  
+      <p>Sim, eu sei que o time do interior paulista era cotado para segurar a laterna do campeonato brasileiro, justamente em seu ano de acesso. Seus inúmeros jogadores que não serviam em outros clubes e por sua falta de tradição o fizeram menosprezado, mas o futebol não é uma matemática exata.</p>
+      <p>O Leão da Alta Araraquarense completa seus 100 anos de história em 2025 e como presente mais que especial ao seu apaixonado torcedor está jogando a elite do campeonato nacional, e muito além disso, superando expectativas.</p>
+      <p>Tudo se iniciou através de Luiz Araújo, jovem promessa da base vendida ao São Paulo, que em 2018 foi vendido pelo tricolor ao Lille-FRA. O time do interior manteve 20% de uma futura venda do atleta que lhe rendeu na época 9 milhões de reais.</p>
+      <p>Do montante, 6,5 milhões foram investidos em seu CT de alta performance e cuidado científico com seus atletas e staff. O espaço conta com 4 campos oficiais, aparelhos de última geração, piscinas, apartamentos para concentração e até mesmo de uma cápsula de flutuação onde os atletas flutuam em uma água morna salina que tem como principais benefícios redução de estresse, alívio de dores e melhorias no sono e bem-estar.</p>
+      <h2>A Força do Coletivo e a Gestão como Diferencial</h2>
+      <p align="center">
+        <img src="/mirassol.jpg" alt="Estrutura e CT do Mirassol Futebol Clube" style="width: 100%; max-width: 550px; height: auto; margin: 20px auto; display: block; border-radius: 8px;">
+      </p>
+      <p>O time vem fazendo uma campanha histórica na série mas algo me chama muito atenção: a quantidade de jogadores em final de contrato - são incríveis 15 jogadores com contratos até o final de 2025 entre vínculos definitivos e empréstimos. O veterano lateral-esquerdo Reinaldo é um caso deles, o camisa 6 soma 8 gols e 3 assistências e tem sido o principal destaque despontando até como o melhor da posição no campeonato, o seu contrato se encerra em 31/12/25.</p>
+      <p>O técnico Rafael Guanaes tem parcela crucial no desempenho do time, o paulista da capital tem um jogo muito bem construído apartir do 4-2-3-1 de muita eficiência tática, jogo aproximado e principalmente de DNA ofensivo, o time tem o 2⁰ melhor ataque da competição superando clubes como Palmeiras e Botafogo, figura também entre os times que mais criam grandes chances, sendo o 5⁰ no geral.</p>
+      <p>Acima de tudo, lá atrás, o Mira entendeu que cuidar dos seus atletas e blindar seu staff técnico seria crucial para sonhar e desempenhar dentro de campo. O cuidado com a parte física e mental dos atletas e da comissão fez e faz total diferença, o time da cidade de 65 mil habitantes fez do profissionalismo o seu melhor aliado. O futuro é promissor em terras Mirassolenses.</p>
+    `
+  },
+  {
+    slug: 'maturidade-no-futebol-jose-lopez',
+    title: 'O FUTEBOL E SUA MATURIDADE ⏳',
+    subtitle: 'O caso José "Flaco" López',
+    category: 'Desempenho de atleta',
+    date: '19 de Setembro de 2025',
+    author: 'Marcos Lobo',
+    content: `
+      <p>No mundo esportivo - especialmente no futebol - a maturação de um jogador exige tempo e paciência. Somos um país rico em jovens promessas latinas, que muitas vezes migram cedo, carregando em si responsabilidades exageradas.</p>
+      <p>Enxergar o potencial de um jovem não é o desafio principal. O verdadeiro desafio está em preparar, lapidar e desenvolver esse potencial até que ele se transforme em um atleta de alto desempenho. Abel Ferreira soube conduzir esse processo com José López, o "Flaco".</p>
+      <h2>O processo de adaptação de "Flaco" López</h2>
+      <p align="center">
+      <img src="/flaco.png" alt="Imagem do jogador Flaco Lopez" style="width: 100%; max-width: 550px; height: auto; margin: 20px auto; display: block; border-radius: 8px;">
+      </p>
+      </p>
+      <p>O argentino chegou ao Brasil em 2022, cercado de expectativa pelas cifras investidas e pela carência do torcedor passional, por um “salvador”. Mas o processo não foi imediato.</p>
+      <p>Em entrevista recente, João Martins, auxiliar de Abel, revelou as dificuldades de adaptação do argentino: sono desregulado, alimentação desbalanceada e até problemas de concentração/atenção.</p>
+      <p>Após um período de instabilidade, 2025 se tornou seu ano de afirmação. Adaptado e atuando em nova função, soma 17 gols e 3 assistências na temporada.</p>
+      <p>Qualidade ele sempre teve. Faltava o que muitos ignoram: tempo, maturação e paciência. Nós, que analisamos o futebol além da emoção, temos a responsabilidade de ajudar o público a compreender o jogo com mais profundidade.</p>
+      <p>O cuidado importa. A conversa importa. E acima de tudo: confiar no produto e no processo.</p>
+    `
+  },
+  {
     slug: 'o-jogo-sem-bola-futebol-moderno',
     title: 'O JOGO SEM BOLA 🚫⚽',
     subtitle: 'A importância do jogo sem bola no futebol moderno',
@@ -21,47 +83,5 @@ export const posts = [
       <p>Hoje, vencer cada duelo, cada metro, é vencer a partida. Christian faz dos seus 88 minutos sem a bola algo crucial para o coletivo. O talento ainda importa, mas o trabalho que poucos enxergam é o que sustenta o alto nível no longo prazo.</p>
     `
   },
-  {
-    slug: 'maturidade-no-futebol-jose-lopez',
-    title: 'O FUTEBOL E SUA MATURIDADE ⏳',
-    subtitle: 'O caso José "Flaco" López',
-    category: 'Desenvolvimento de Atletas',
-    date: '19 de Setembro de 2025',
-    author: 'Marcos Lobo',
-    content: `
-      <p>No mundo esportivo - especialmente no futebol - a maturação de um jogador exige tempo e paciência. Somos um país rico em jovens promessas latinas, que muitas vezes migram cedo, carregando em si responsabilidades exageradas.</p>
-      <p>Enxergar o potencial de um jovem não é o desafio principal. O verdadeiro desafio está em preparar, lapidar e desenvolver esse potencial até que ele se transforme em um atleta de alto desempenho. Abel Ferreira soube conduzir esse processo com José López, o "Flaco".</p>
-      <h2>O processo de adaptação de "Flaco" López</h2>
-      <p align="center">
-      <img src="/flaco.png" alt="Imagem do jogador Flaco Lopez" style="width: 100%; max-width: 550px; height: auto; margin: 20px auto; display: block; border-radius: 8px;">
-      </p>
-      </p>
-      <p>O argentino chegou ao Brasil em 2022, cercado de expectativa pelas cifras investidas e pela carência do torcedor passional, por um “salvador”. Mas o processo não foi imediato.</p>
-      <p>Em entrevista recente, João Martins, auxiliar de Abel, revelou as dificuldades de adaptação do argentino: sono desregulado, alimentação desbalanceada e até problemas de concentração/atenção.</p>
-      <p>Após um período de instabilidade, 2025 se tornou seu ano de afirmação. Adaptado e atuando em nova função, soma 17 gols e 3 assistências na temporada.</p>
-      <p>Qualidade ele sempre teve. Faltava o que muitos ignoram: tempo, maturação e paciência. Nós, que analisamos o futebol além da emoção, temos a responsabilidade de ajudar o público a compreender o jogo com mais profundidade.</p>
-      <p>O cuidado importa. A conversa importa. E acima de tudo: confiar no produto e no processo.</p>
-    `
-  },
-  { 
-    slug: 'o-improvavel-mirassol',
-    title: 'O IMPROVÁVEL MIRASSOL 🌞',
-    subtitle: 'A ascensão surpreendente do Mirassol no futebol brasileiro',
-    category: 'Análise de Clubes',
-    date: '26 de Setembro de 2025',
-    author: 'Marcos Lobo',
-    content: `
-      <p>Sim, eu sei que o time do interior paulista era cotado para segurar a laterna do campeonato brasileiro, justamente em seu ano de acesso. Seus inúmeros jogadores que não serviam em outros clubes e por sua falta de tradição o fizeram menosprezado, mas o futebol não é uma matemática exata.</p>
-      <p>O Leão da Alta Araraquarense completa seus 100 anos de história em 2025 e como presente mais que especial ao seu apaixonado torcedor está jogando a elite do campeonato nacional, e muito além disso, superando expectativas.</p>
-      <p>Tudo se iniciou através de Luiz Araújo, jovem promessa da base vendida ao São Paulo, que em 2018 foi vendido pelo tricolor ao Lille-FRA. O time do interior manteve 20% de uma futura venda do atleta que lhe rendeu na época 9 milhões de reais.</p>
-      <p>Do montante, 6,5 milhões foram investidos em seu CT de alta performance e cuidado científico com seus atletas e staff. O espaço conta com 4 campos oficiais, aparelhos de última geração, piscinas, apartamentos para concentração e até mesmo de uma cápsula de flutuação onde os atletas flutuam em uma água morna salina que tem como principais benefícios redução de estresse, alívio de dores e melhorias no sono e bem-estar.</p>
-      <h2>A Força do Coletivo e a Gestão como Diferencial</h2>
-      <p align="center">
-        <img src="/mirassol.jpg" alt="Estrutura e CT do Mirassol Futebol Clube" style="width: 100%; max-width: 550px; height: auto; margin: 20px auto; display: block; border-radius: 8px;">
-      </p>
-      <p>O time vem fazendo uma campanha histórica na série mas algo me chama muito atenção: a quantidade de jogadores em final de contrato - são incríveis 15 jogadores com contratos até o final de 2025 entre vínculos definitivos e empréstimos. O veterano lateral-esquerdo Reinaldo é um caso deles, o camisa 6 soma 8 gols e 3 assistências e tem sido o principal destaque despontando até como o melhor da posição no campeonato, o seu contrato se encerra em 31/12/25.</p>
-      <p>O técnico Rafael Guanaes tem parcela crucial no desempenho do time, o paulista da capital tem um jogo muito bem construído apartir do 4-2-3-1 de muita eficiência tática, jogo aproximado e principalmente de DNA ofensivo, o time tem o 2⁰ melhor ataque da competição superando clubes como Palmeiras e Botafogo, figura também entre os times que mais criam grandes chances, sendo o 5⁰ no geral.</p>
-      <p>Acima de tudo, lá atrás, o Mira entendeu que cuidar dos seus atletas e blindar seu staff técnico seria crucial para sonhar e desempenhar dentro de campo. O cuidado com a parte física e mental dos atletas e da comissão fez e faz total diferença, o time da cidade de 65 mil habitantes fez do profissionalismo o seu melhor aliado. O futuro é promissor em terras Mirassolenses.</p>
-    `
-  }
+
 ];
